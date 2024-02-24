4355ad86e31855f8d974717e36941defe6f89a36
Erivando Sena <erivandosena@gmail.com>
2024-02-24 15:33:14 -0300

    fixed of function to environment colab

--- StartJupyterNotebook.cmd
+++ StartJupyterNotebook.cmd
@@ -1,3 +1,4 @@
 @echo off
+:: Inicia o Jupyter Notebook
 jupyter trust ./DataScraping-SSPDS-CE.ipynb
 jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com/' --port=8888 --NotebookApp.port_retries=0 --NotebookApp.kernel_name=r ./DataScraping-SSPDS-CE.ipynb
